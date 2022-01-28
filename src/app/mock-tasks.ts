2396aed923b9d0c406467d5b34c35178ac949287
Sebastian <68450501+teshy18@users.noreply.github.com>
2022-01-28 17:46:34 -0300

    Se agrego la interface de Task y el mock de tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task = [
+  {
+    id:1,
+    tarea: "Terminar el proyecto 'Lista de Tareas'",
+    fecha: "31 de Enero",
+    recordatorio: true,
+  },
+  {
+    id:2,
+    tarea: "Comprar insumos de oficina",
+    fecha: "27 de Enero",
+    recordatorio: true,
+  },
+  {
+    id:3,
+    tarea: "Llevar el auto al taller",
+    fecha: "12 de Febrero",
+    recordatorio: false,
+  }
+]
