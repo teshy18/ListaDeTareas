234a45bb8d0560492f51bbb9c7649691f84bd8c2
Sebastian <68450501+teshy18@users.noreply.github.com>
2022-01-29 01:15:45 -0300

    se agrego fontawesome mediant ng add

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Task} from '../../Task'
 import { TASKS } from 'src/app/mock-tasks';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -15,6 +16,8 @@ export class TaskItemComponent implements OnInit {
     fecha:"asdasd",
     recordatorio:true
   };
+  faCoffee = faCoffee;
+  
   constructor() { }
 
   ngOnInit(): void {
